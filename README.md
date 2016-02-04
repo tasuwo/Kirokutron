4cc424c1fb207c512e09fb3bb31074611fd7b2bf
tasuwo <kamuhata.you@gmail.com>
2016-02-04 11:17:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+#Kirokutron
+
+##これはなに
+
+##つかいかた
+
+以下のような `node_modules/my_modules/key.js` を作成する．
+
+``` javascript
+exports.CONSUMER_KEY = /* コンシューマキー */;
+exports.CONSUMER_SECRET = /* コンシューマシークレット */;
+```
+
+mongoDB を利用するので準備しておく．
+
+```bash
+$ brew install mongodb
+```
+
+依存パッケージをインストールする．
+
+```bash
+$ npm install
+```
---
+++ main.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var twitterAPI = require('node-twitter-api');
+var reporter = require('crash-reporter');
+var stream = require('my_modules/stream');
+var key = require('my_modules/key');
+
+reporter.start();
+
+var mainWindow = null;
+
+var TWITTER_CONSUMER_KEY = key.CONSUMER_KEY;
+var TWITTER_CONSUMER_SECRET = key.CONSUMER_SECRET;
+var ACCESS_TOKEN_KEY = '';
+var ACCESS_TOKEN_SECRET = '';
+
+app
+  .on('ready', getRequest)
+  .on('window-all-closed', function() { if (process.platform != 'darwin') app.quit();});
+
+function getRequest() {
+  var twitter = new twitterAPI({
+    consumerKey: TWITTER_CONSUMER_KEY,
+    consumerSecret: TWITTER_CONSUMER_SECRET,
+    callback: 'https://www.google.co.jp/'
+  });
+
+  twitter.getRequestToken(function(error, requestToken, requestTokenSecret) {
+    var url = twitter.getAuthUrl(requestToken);
+    var loginWindow = new BrowserWindow({width: 800, height: 600});
+
+    // TODO : 認証失敗時の処理の追加
+    loginWindow.webContents.on('will-navigate', function (preventDefault, url) {
+      var matched;
+      if(matched = url.match(/\?oauth_token=([^&]*)&oauth_verifier=([^&]*)/)) {
+        twitter.getAccessToken(
+          requestToken, requestTokenSecret, matched[2],
+          function(error, accessToken, accessTokenSecret){
+            console.log('OAuth succeeded!');
+            ACCESS_TOKEN_KEY = accessToken;
+            ACCESS_TOKEN_SECRET = accessTokenSecret;
+            getMainWindow();
+          });
+      }
+
+      setTimeout(function () {
+        loginWindow.close();
+      }, 0);
+    });
+
+    loginWindow.loadUrl(url);
+  });
+}
+
+function getMainWindow() {
+  mainWindow = new BrowserWindow({ 'width': 1024, 'height': 600 });
+  mainWindow.loadUrl('file://' + __dirname + '/view/index.html');
+
+  // TODO : DB状況を非同期にアップデートして view に表示する
+  stream.streaming(
+    TWITTER_CONSUMER_KEY,
+    TWITTER_CONSUMER_SECRET,
+    ACCESS_TOKEN_KEY,
+    ACCESS_TOKEN_SECRET
+  );
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
